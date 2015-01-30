6d254ca4fdd91b7a8af641074438d2f1203c3c32
Gregory Jefferis <jefferis@gmail.com>
2015-01-29 20:58:31 -0800

    Fix up quickhash docs

--- R/quickhash.R
+++ R/quickhash.R
@@ -1,14 +1,15 @@
-#' Compute a quick hash for head and tail of an lsm file
-#'
-#' The first ~ 150,000 bytes includes a number of fields such as
-#' dimension and acquisition timestamp that are typically enough
-#' to make for a unique hash. In particular the first 150K (but not 100K) are
-#' unique across the 16226 v2 flycircuit LSMs. The hope is the last 100K
-#' should include some image data (though in practice for flycrircuit LSMs)
-#' this is often in the 8 bit segmented neurons which nearly all have 0 background
+#' Compute a quick hash for head and tail of a Zeiss LSM confocal file
+#' 
+#' The first ~ 150,000 bytes includes a number of fields such as dimension and
+#' acquisition timestamp that are typically enough to make for a unique hash. In
+#' particular the first 150K (but not 100K) are unique across the 16226 v2
+#' flycircuit LSMs. The hope is the last 100K should include some image data
+#' (though in practice for flycrircuit LSMs) this is often in the 8 bit
+#' segmented neurons which nearly all have 0 background
 #' @param f Path to lsm files
 #' @param offsets Default c(0,-1e5)
 #' @param chunksizes Default c(1e5,1e5)
+#' @param ... Additional arguments passed to default \code{\link{quickhash}} function
 #' @return character vector containing hash values for lsm(s)
 #' @export
 #' @seealso \code{\link{quickhash}} and \code{\link{digest}}
@@ -17,17 +18,21 @@ quickhash.lsm<-function(f,offsets=c(0,-1e5),chunksizes=c(1.5e5,1e5),...){
 }
 
 #' Compute a (quick) hash for some part of the contents of a set of files
-#'
-#' Default is to compute md5 hash for whole of each file.
-#' negative offsets => seek to offset position wrt end of file
-#' chunksize=Inf => whole file
-#' Uses digest() which defaults to algo=md5
+#' 
+#' Default is to compute md5 hash for whole of each file. negative offsets => 
+#' seek to offset position wrt end of file \code{chunksize=Inf} => whole file. 
+#' Uses digest() which defaults to algo="md5"
 #' @param f Path to files to hash
 #' @param offsets integer offsets into file at which to read data
 #' @param chunksizes amount of data to read at each offset
+#' @param CheckFileSize see if the file has enough data to match the 
+#'   requirements defined by \code{offsets} and \code{chunksizes} and compute a 
+#'   digest of the entire file when there is insufficient data. The default 
+#'   value of \code{TRUE} is strongly recommended.
+#' @param ... Additional arguments passed to the \code{\link{digest}} function.
 #' @return character vector containing hash values for file(s)
 #' @export
-#' @seealso \code{\link{digest}}
+#' @seealso \code{\link{digest}}, \code{\link{quickhash.lsm}}
 #' @import digest
 quickhash<-function(f,offsets=0,chunksizes=Inf,CheckFileSize=TRUE,...){
 	if(length(f)>1)
@@ -40,6 +45,7 @@ quickhash<-function(f,offsets=0,chunksizes=Inf,CheckFileSize=TRUE,...){
 		stop("Must specify the same number of offsets (",length(offsets),
 			") and chunk sizes (",length(chunksizes),")")
 	# hash whole file, if smaller than total chunk sizes requested 
+  # FIXME also need to consider offsets as well
 	mindatatohash=sum(chunksizes)
 	if(!is.finite(mindatatohash) || 
 		(CheckFileSize && file.info(f)$size<=mindatatohash))
