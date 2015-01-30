7d3f4cbb675218e07b85d07526f4800526cc3d0d
Gregory Jefferis <jefferis@gmail.com>
2015-01-29 19:39:36 -0800

    init with skeleton package

---
+++ R/jimpipeline-package.R
@@ -0,0 +1,11 @@
+#' Package to wrap Jefferis lab image processing pipeline
+#' 
+#' This will contain functions that we use during our large scale image 
+#' processing and registration work. We will try to make these as generic as
+#' possible, but for the moment use by third parties is not a major design
+#' consideration.
+#' @name jimpipeline-package
+#' @aliases jimpipeline
+#' @docType package
+#' @keywords package
+NULL
