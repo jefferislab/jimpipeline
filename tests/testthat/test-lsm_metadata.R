0f62f92272d7104ec2b87999d9d2b422ed5fd30a
Gregory Jefferis <jefferis@gmail.com>
2016-05-12 12:08:21 +0100

    udpate test expecation

--- tests/testthat/test-lsm_metadata.R
+++ tests/testthat/test-lsm_metadata.R
@@ -2,5 +2,5 @@ context('lsm_metadata')
 
 test_that("we can extract lsm metadata", {
   lsm='test.lsm'
-  expect_match(lsm_metadata(lsm), "VoxelSizeZ: 1.0E-13")
+  expect_match(lsm_metadata(lsm), "VoxelSizeZ: 1.0E-13", all = FALSE)
 })
