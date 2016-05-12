058929d39e959c21df85f751f88012ad2ec3b074
Gregory Jefferis <jefferis@gmail.com>
2016-05-12 12:57:41 +0100

    functions to find / run fiji

---
+++ R/fiji.R
@@ -0,0 +1,65 @@
+
+#' Run a Fiji macro
+#' 
+#' @param macro Path to macro to run
+#' @param macroArg Arguments for macro
+#' @param headless Whether to have ImageJ/Fiji running headless
+#' @param batch Use batch mode
+#' @param MinMem,MaxMem Memory limits
+#' @param IncrementalGC Whether to use incremental garbage collection
+#' @param Threads Number of threads
+#' @param fijiArgs Arguments for ImageJ/Fiji (as opposed to the macro itself)
+#' @param javaArgs Arguments for java
+#' @param ijArgs Arguments for ImageJ
+#' @param fijiPath Path to fiji executable
+#' @param DryRun Whether to return the command to be run rather than actually
+#'   executing it.
+#' @export
+runFijiMacro <- function(macro="",macroArg="",headless=FALSE,batch=TRUE,
+                         MinMem=MaxMem,MaxMem="2500m",IncrementalGC=TRUE,Threads=NULL,
+                         fijiArgs=NULL,javaArgs=NULL,ijArgs=NULL,
+                         fijiPath=fiji(),DryRun=FALSE){
+  
+  if(headless) fijiArgs = c(fijiArgs,"--headless")
+  fijiArgs=paste(fijiArgs,collapse=" ")
+  
+  javaArgs=c(paste("-Xms",MinMem,sep=""),paste("-Xmx",MaxMem,sep=""),javaArgs)
+  if(IncrementalGC) javaArgs=c(javaArgs,"-Xincgc")
+  javaArgs=paste(javaArgs,collapse=" ")
+  
+  threadAdjust=ifelse(is.null(Threads),"",paste("run(\"Memory & Threads...\", \"parallel=",Threads,"\");",sep=""))
+  
+  macroCall=paste(" -eval '",threadAdjust,"runMacro(\"", macro,"\",\"",macroArg,"\");' ",sep="")
+  
+  ijArgs=paste(c(ijArgs,ifelse(batch,"-batch","")),collapse=" ")
+  
+  cmd<-paste(fijiPath,javaArgs,fijiArgs,"--",macroCall,ijArgs)
+  if(DryRun) return(cmd)
+  system(cmd)
+}
+
+ 
+# Private function to eturn path to preferred fiji
+fiji <- function(fijipath=NULL) {
+  if(!is.null(fijipath)) {
+    if(!file.exists(fijipath)) stop("fiji is not at: ", fijipath)
+    options(jimpipeline.fiji=fijipath)
+  } else {
+    # do we have an option set?
+    fijipath=getOption('jimpipeline.fiji')
+    if(!is.null(fijipath)) {
+      if(!file.exists(fijipath))
+        stop("fiji is not at: ", fijipath, " as specificed by options('jimpipeline.fiji')!")
+      else {
+        options(jimpipeline.fiji=NULL)
+      }
+    } else {
+      # look for it in sensible places
+      if(nzchar(fijipath <- Sys.which('fiji'))){
+        options(jimpipeline.fiji=fijipath)
+      }
+      else stop("Unable to find fiji!")
+    }
+  }
+  fijipath
+}
--- R/jimpipeline-package.R
+++ R/jimpipeline-package.R
@@ -1,9 +1,16 @@
 #' Package to wrap Jefferis lab image processing pipeline
 #' 
-#' This will contain functions that we use during our large scale image 
-#' processing and registration work. We will try to make these as generic as
-#' possible, but for the moment use by third parties is not a major design
-#' consideration.
+#' This contains functions that we use during our large scale image processing
+#' and registration work. We will try to make these as generic as possible, but
+#' for the moment use by third parties is not a major design consideration.
+#' @section Package Options: The following options can be set to specify default
+#'   behaviour.
+#'   
+#'   \itemize{
+#'   
+#'   \item{\code{jimpipeline.fiji}}{Location of fiji to use for running scripts}
+#'   
+#'   }
 #' @name jimpipeline-package
 #' @aliases jimpipeline
 #' @docType package
