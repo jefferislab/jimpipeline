55835f565f582a6c40076c8454e9355d6e9ff64d
Gregory Jefferis <jefferis@gmail.com>
2017-12-10 08:37:01 +0000

    teach convertlsmstonrrd to use different channel orders
    * closes #2

--- R/lsm_convert.R
+++ R/lsm_convert.R
@@ -1,11 +1,16 @@
 #' Split Zeiss LSMs and save each channel to a nrrd
-#' 
+#'
 #' image.lsm -> image_01.nrrd image_02.nrrd image_03.nrrd
-#' 
-#' Uses fiji and BatchConvertFlycircuitLSMsToNrrds macro, which in turn relies
-#' on LOCI Bio-Formats which cannot currently be run headless.
-#' 
-#' Uses RunCmdForNewerInput to compare in/outputs to see if action required. 
+#'
+#' ChannelOrder specifies the output stem of the channels in the input image.
+#' Thus \code{c(2,3,1)} would imply:
+#'
+#' image.lsm[1:3] -> image_02.nrrd image_03.nrrd image_01.nrrd
+#'
+#' \code{lsmstoconvert} uses Fiji and lsmtonrrd macro, which in turn relies on
+#' LOCI Bio-Formats which cannot currently be run headless.
+#'
+#' Uses RunCmdForNewerInput to compare in/outputs to see if action required.
 #' Uses file locking so can be run in parallel, although IO intensive so running
 #' too many jobs in parallel would be counter-productive. RandomOrder=TRUE
 #' reduces file locking pressure when several processes compete to lock the same
@@ -13,7 +18,9 @@
 #' @param lsmstoconvert Paths to one or more LSMs or a single directory
 #'   containing LSMs
 #' @param rawnrrdsdir Output directory in which to save nrrds
-#' @param RandomOrder Scramble processing order (default TRUE)
+#' @param ChannelOrder The order in which channels of each individual image are
+#'   saved (defaults to swapping the order of first 2 channels - see details).
+#' @param RandomOrder Scramble processing order of files (default TRUE)
 #' @param DryRun Show what would happen but don't run (default TRUE)
 #' @param Verbose Show filename (default !DryRun)
 #' @param ... Additional arguments passed to \code{\link{runFijiMacro}}
@@ -21,7 +28,14 @@
 #'   file
 #' @export
 #' @seealso \code{\link{runFijiMacro}}, \code{\link{RunCmdForNewerInput}}
-convertlsmstonrrd<-function(lsmstoconvert, rawnrrdsdir,
+#' @examples
+#' \dontrun{
+#' convertlsmstonrrd("my.lsm", ChannelOrder=1:3, DryRun=FALSE)
+#' }
+#'
+#' # Path to the Fiji macro distributed with the package
+#' system.file('ijm','lsmtonrrd.txt', package = 'jimpipeline')
+convertlsmstonrrd<-function(lsmstoconvert, rawnrrdsdir, ChannelOrder=c(2,1,3,4),
   RandomOrder=TRUE,DryRun=TRUE,Verbose=!DryRun, ...){
   
   if(!file.exists(rawnrrdsdir)) dir.create(rawnrrdsdir)
@@ -33,11 +47,19 @@ convertlsmstonrrd<-function(lsmstoconvert, rawnrrdsdir,
   runsummary=logical(length(lsmstoconvert))
   names(runsummary)=basename(lsmstoconvert)
   
+  missingChannels=setdiff(seq_len(max(ChannelOrder)), ChannelOrder)
+  if(length(missingChannels)) {
+    stop("You have not specified the channel order for channels: ", 
+         paste(missingChannels, collapse = ', '))
+  }
+  # c(2,1,3,4) -> "02010304" for ImageJ
+  ChannelOrder=paste0("0", ChannelOrder, collapse = "")
+  
   for(i in seq(along=lsmstoconvert)){
     f=lsmstoconvert[i]
     cmd=runFijiMacro(
       macro=system.file('ijm','lsmtonrrd.txt', package = 'jimpipeline'),
-      macroArg=paste(f, rawnrrdsdir, 'nrrd',sep=","),
+      macroArg=paste(f, rawnrrdsdir, 'nrrd', ChannelOrder, sep=","),
       # javaArgs="-noverify",
       headless=TRUE, DryRun=TRUE, ...)
     gene_name=sub("\\.lsm","",basename(f))
